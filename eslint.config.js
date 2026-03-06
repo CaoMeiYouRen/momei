220ee25abc0172f6b5ee19c10171f33bafc7b989
CaoMeiYouRen <caomeiyouren@qq.com>
2026-03-06 23:47:29 +0800

    chore(eslint): 添加 vue-i18n 插件配置 - 引入 @intlify/eslint-plugin-vue-i18n 插件 - 配置 vue-i18n 规则以支持国际化 - 关闭未使用键的警告，启用动态键的错误提示

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import cmyrConfig from 'eslint-config-cmyr/nuxt'
 import { globalIgnores } from 'eslint/config'
 import pluginVue from 'eslint-plugin-vue'
+import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
@@ -30,17 +31,35 @@ export default withNuxt(
             // tseslint,
         },
     },
+    ...vueI18n.configs.recommended,
+    {
+        files: ['**/*.json'],
+        rules: {
+            '@intlify/vue-i18n/no-unused-keys': 'off',
+        },
+    },
+    {
+        rules: {
+            '@intlify/vue-i18n/no-dynamic-keys': 'off',
+            '@intlify/vue-i18n/no-unused-keys': [
+                'warn',
+                {
+                    extensions: ['.js', '.ts', '.vue'],
+                },
+            ],
+        },
+        settings: {
+            'vue-i18n': {
+                localeDir: './i18n/locales/*.json',
+                messageSyntaxVersion: '^10.0.0',
+            },
+        },
+    },
     cmyrConfig,
     {
         rules: {
             'max-lines': [1, { max: 800 }], // 强制文件的最大行数
             'max-lines-per-function': [0, { max: 150 }], // 强制函数最大行数
-        },
-    },
-    {
-        files: ['pages/admin/posts/[[]id[]].vue'],
-        rules: {
-            'max-lines': 0,
         },
     },
 )
